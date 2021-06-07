14463ac5c4d5b5bf9b80af18c7d59971c7d1097f
kareem saeed <karimsaid026@gmail.com>
2021-06-07 14:16:41 +0200

    Search For Flight Added (Fully Done)

--- Flight Reservation system/Flight Reservation system/Forms/Search For Flight.Designer.cs
+++ Flight Reservation system/Flight Reservation system/Forms/Search For Flight.Designer.cs
@@ -28,21 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmb_1 = new System.Windows.Forms.ComboBox();
+            this.cmb_2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::Flight_Reservation_system.Properties.Resources.index1;
+            this.pictureBox2.Location = new System.Drawing.Point(742, 7);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(47, 52);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(288, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "To:";
+            // 
+            // cmb_1
+            // 
+            this.cmb_1.FormattingEnabled = true;
+            this.cmb_1.Location = new System.Drawing.Point(88, 80);
+            this.cmb_1.Name = "cmb_1";
+            this.cmb_1.Size = new System.Drawing.Size(159, 21);
+            this.cmb_1.TabIndex = 14;
+            // 
+            // cmb_2
+            // 
+            this.cmb_2.FormattingEnabled = true;
+            this.cmb_2.Location = new System.Drawing.Point(320, 81);
+            this.cmb_2.Name = "cmb_2";
+            this.cmb_2.Size = new System.Drawing.Size(159, 21);
+            this.cmb_2.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 127);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(743, 334);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.Color.Chocolate;
+            this.button1.Location = new System.Drawing.Point(523, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 30);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Chocolate;
+            this.label3.Location = new System.Drawing.Point(22, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 24);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Search For Flight";
             // 
             // Search_For_Flight
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(796, 473);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmb_2);
+            this.Controls.Add(this.cmb_1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Search_For_Flight";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Search_For_Flight";
             this.Load += new System.EventHandler(this.Search_For_Flight_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmb_1;
+        private System.Windows.Forms.ComboBox cmb_2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Flight Reservation system/Flight Reservation system/Forms/Search For Flight.cs
+++ Flight Reservation system/Flight Reservation system/Forms/Search For Flight.cs
@@ -14,6 +14,13 @@ namespace Flight_Reservation_system
 {
     public partial class Search_For_Flight : Form
     {
+        OracleConnection conn;
+        OracleDataAdapter da;
+        OracleCommandBuilder cb;
+        DataSet ds;
+
+        string ordb = "Data source=orcl;User Id=scott; Password=tiger;";
+
         public Search_For_Flight()
         {
             InitializeComponent();
@@ -21,7 +28,56 @@ namespace Flight_Reservation_system
 
         private void Search_For_Flight_Load(object sender, EventArgs e)
         {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "PRO_get_city";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("cities", OracleDbType.RefCursor, ParameterDirection.Output);
+            OracleDataReader orc = cmd.ExecuteReader();
+            List<string> stringtxt = new List<string>();
+            while (orc.Read())
+            {
+                //Console.WriteLine(orc[0]);
+                stringtxt.Add((string)orc[0]);
+            }
+            List<string> distinct = stringtxt.Distinct().ToList();
+            foreach (string value in distinct)
+            {
+                cmb_1.Items.Add(value);
+                cmb_2.Items.Add(value);
+            }
+            orc.Close();
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cmb_1.Text.Equals("") || cmb_2.Text.Equals(""))
+            {
+                MessageBox.Show("Empty Cell!!!");
+            }
+            else
+            {
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "PRO_SEARCH_FOR_Flight";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("sourcee", cmb_1.SelectedItem.ToString());
+                cmd.Parameters.Add("des", cmb_2.SelectedItem.ToString());
+                cmd.Parameters.Add("cities", OracleDbType.RefCursor, ParameterDirection.Output);
+                da = new OracleDataAdapter(cmd);
+                cb = new OracleCommandBuilder(da);
+                ds = new DataSet();
+                da.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+                //conn.Close();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
