792e1ae6f92084e9ad9458e3fbed480820be217a
kareem saeed <karimsaid026@gmail.com>
2021-06-14 01:55:38 +0200

    Deleted Repeated Folder

--- Flight Reservation system/Database/Stored Procedures/Get City.sql
+++
@@ -1,9 +0,0 @@
--- Create Get City Stored Procedures
-create or replace PROCEDURE PRO_get_city (out_city out sys_refcursor)
-AS
-BEGIN
-open out_city for
-  select name
-  from pro_airport;
-
-end PRO_get_city;
